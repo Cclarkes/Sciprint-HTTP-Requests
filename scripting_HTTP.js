f787fbf3b478bef8a8c87255f0d3ab863ab118a1
CClarkes <connorclarkes@gmail.com>
2019-02-11 20:35:42 +0000

    step 1 fixed

--- scripting_HTTP.js
+++ scripting_HTTP.js
@@ -1,7 +1,6 @@
 const https = require('https');
-var chunkList = "";
 
-function getAndPrintHTMLChunks (cb) {
+function getAndPrintHTMLChunks () {
     var requestOptions = {
       host: 'sytantris.github.io',
       path: '/http-examples/step1.html'
@@ -10,19 +9,13 @@ function getAndPrintHTMLChunks (cb) {
   https.get(requestOptions,function (response) {
   response.setEncoding('utf8');
   response.on('data', function (data) {
-      cb(response)
     console.log('Chunk Received. Length:', data.length);
+    console.log(data + "\n");
   });
   response.on('end', function(end) {
-    console.log(chunkList);
     console.log('Response stream complete.');
   });
   })
 }
-  function concatenator(response) {
-    for(data in response) {
-       chunkList += ("\n" + data);
-    }
-  }
 
-  getAndPrintHTMLChunks(concatenator);
+  getAndPrintHTMLChunks();
